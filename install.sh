4fa7e9bf541b6c40c92e9a174f20c7b00c6c9865
Chandrappa Menasinahal <chandrapp.menasinahal@privafy.com>
2022-12-21 15:15:14 +0530

    Added installer script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if ! which nvim 
+then
+	echo "Neovim not present installing"
+	curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+	sudo dpkg -i nvim-linux64.deb
+	sudo apt-get install python3-neovim
+fi
+git clone https://github.com/smpchandru/cnvim.git
+cp -r cnvim/nvim $HOME/.config/nvim
+nvim -c ":PackerSync"
+
